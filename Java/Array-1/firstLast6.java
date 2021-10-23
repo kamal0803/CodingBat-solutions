3ca8ec5d60cd6965e726bc4a682660702896693b
Kamal <kamal0803@gmail.com>
2021-10-23 13:13:37 +0530

    firstLast6.java updated of Array-1 in Java

--- Java/Array-1/firstLast6.java
+++ Java/Array-1/firstLast6.java
@@ -9,8 +9,5 @@ firstLast6([13, 6, 1, 2, 3]) ? false
 */
 
 public boolean firstLast6(int[] nums) {
-  if(nums[0]==6 || nums[nums.length-1]==6)
-    return true;
-  else
-    return false;
+  return (nums[0]==6 || nums[nums.length-1]==6);
 }
